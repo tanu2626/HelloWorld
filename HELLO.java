6dd7d1cb150eac3700a0597fe3923a49f751e844
Tanu Sharma <tsharma@aconex.com>
2017-07-21 13:47:21 +1000

    Added helloworld project

---
+++ HELLO.java
@@ -0,0 +1,13 @@
+
+// Create a class
+public class HELLO{
+  public static void main(String[] args) {
+
+// Print hello world 10 times
+    for(int i=1;i<=10;i++){
+
+      System.out.println("Hello World.."+i);
+
+    }
+  }
+}
